15458a6c806f7d3e204b80c6fb04f3eca980a78e
tahir.abuzetoglu <extern.tahir-abuzetoglu@allianz.com.tr>
2022-03-20 13:50:20 +0300

    fet: Updated gradle.

---
+++ app/src/main/java/com/tahirikosan/pokemonnft/utils/FirebaseUtils.kt
@@ -0,0 +1,13 @@
+package com.tahirikosan.pokemonnft.utils
+
+object FirebaseUtils {
+    const val COLLECTION_ROOMS = "rooms"
+    const val COLLECTION_QUEUE= "queue"
+    const val COLLECTION_USERS = "users"
+    const val QUEUE_DOC_IC = "iji04WUR6e6Wq5ulOBFl"
+    const val field_health = "health"
+    const val field_turn = "turn"
+    const val field_pvp = "pvp"
+    const val field_players = "players"
+    const val field_coin= "coin"
+}
