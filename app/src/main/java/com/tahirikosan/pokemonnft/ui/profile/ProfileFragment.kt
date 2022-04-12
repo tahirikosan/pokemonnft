6654514f2296af2d1f35904813c90a8e415d45cb
tahir.abuzetoglu <extern.tahir-abuzetoglu@allianz.com.tr>
2022-04-12 15:35:12 +0300

    fet: Add mnemonic to user profile .

--- app/src/main/java/com/tahirikosan/pokemonnft/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/tahirikosan/pokemonnft/ui/profile/ProfileFragment.kt
@@ -1,9 +1,12 @@
 package com.tahirikosan.pokemonnft.ui.profile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.tahirikosan.pokemonnft.R
 import com.tahirikosan.pokemonnft.base.BaseFragment
 import com.tahirikosan.pokemonnft.data.remote.Resource
 import com.tahirikosan.pokemonnft.databinding.FragmentProfileBinding
@@ -15,6 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
 
     private val viewModel: ProfileViewModel by viewModels()
+    private var isMnemonicVisible = false
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleClicks()
@@ -24,7 +28,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
     }
 
     private fun setViews() {
-        with(binding){
+        with(binding) {
             tvPublicKey.text = userPreferences.publicKey
         }
     }
@@ -57,6 +61,28 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBind
             btnConnectToWallet.setOnClickListener {
                 findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToWalletConnectionFragment())
             }
+
+            ivToggleMnemonic.setOnClickListener {
+                toggleMnemonic()
+            }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun toggleMnemonic() {
+        if (isMnemonicVisible) {
+            binding.ivToggleMnemonic.setImageResource(
+                R.drawable.ic_baseline_lock_24
+            )
+            // Corrupt mnemonic view
+            binding.tvMnemonic.text = "Click lock image to unlock mnemonic text."
+        } else {
+            binding.ivToggleMnemonic.setImageResource(
+                R.drawable.ic_baseline_lock_open_24
+            )
+            // Corrupt mnemonic view
+            binding.tvMnemonic.text = "Phrase: ${userPreferences.getMnemonic().phrase} \nPath: ${userPreferences.getMnemonic().path} \nLocale: ${userPreferences.getMnemonic().locale}"
+        }
+        isMnemonicVisible = !isMnemonicVisible
     }
 }
